e86cfd1dc52c2197ad73688b7b7473c19c33dff9
vinobc <vinobchanderr@gmail.com>
2025-03-12 03:38:29 +0000

    Increase the body-parser limit in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,8 +18,8 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use("/api/settings", systemSettingRoutes);
 
 // Database connection
